f00331ef9ba3cb64c2d32e2753cddf8cc5d89193
NikolVasileva <nikol.vasileva9@gmail.com>
2025-09-22 12:53:06 +0300

    Setup Handlebars

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ SoftUni JS Back-End Course Project Part 1
 
 Development Steps
 1. Initialize Project
-2. Add Express Server
+2. Add Express Server -> "npm i express"
 3. Add workshop resources
-4. Setup Handlebars
+4. Setup Handlebars -> "npm i express-handlebars"
 5. Render Home Page
--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
 import express from "express"
+import handlebars from "express-handlebars"
 
 const app = express();
 
+//1. Setup Handlebars
+/// 1.1 setup view engine
+app.engine("hbs", handlebars.engine({
+    extname: "hbs",
+}));
+
+/// 1.2. setup view engine that will be used
+app.set("view engine", "hbs")
+
+/// 1.3. setup where is location of the folder views
+app.set("views", "src/views")
+
+// 2. Routes
 app.get("/", (req, res) => {
-    res.send("It works!");
+    // res.send("It works!");
+    res.render("home", {layout: false})
 });
 
+// 3. Start Server
 app.listen(3000, () => console.log("Server is listening to http://localhost:3000..."))
