f6066819838c231d978bf6d376714586dc8c5419
NikolVasileva <nikol.vasileva9@gmail.com>
2025-09-22 15:06:18 +0300

    Add movie service

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,10 +1,14 @@
 import { Router } from "express";
+import movieService from "../services/movieService.js";
 
 const homeController = Router();
 
 // 2. Routes
 homeController.get("/", (req, res) => {
     // res.send("It works!");
+    const movies = movieService.getAll()
+    console.log(movies)
+
     res.render("home")
 });
 
